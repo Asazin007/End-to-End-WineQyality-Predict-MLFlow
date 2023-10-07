db1b02be47cdb5da3f3b66fb4fb80e7ecfb75bdd
Asazin007 <asim80822@gmail.com>
2023-10-07 17:05:15 +0530

    updated structure

--- template.py
+++ template.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.INFO)
 project_name="ml_project"
 
 list_of_files=[
-    # ".github/workflows/.gitkeep",
+    ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__ini__.py",
     f"src/{project_name}/components/data_ingestion.py",
@@ -17,9 +17,23 @@ list_of_files=[
     f"src/{project_name}/pipelines/__init__.py",
     f"src/{project_name}/pipelines/training_pipeline.py",
     f"src/{project_name}/pipelines/prediction_pipeline.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/exception.py",
     f"src/{project_name}/logger.py",
+     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
     "app.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "test.py"
     "Dockerfile",
     "requirements.txt",
     "setup.py"   
